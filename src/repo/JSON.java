11edf32b81bf433f2c096cad770be275904ca211
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-10-05 18:38:15 +1300

    json reader added

--- src/repo/JSON.java
+++ src/repo/JSON.java
@@ -7,19 +7,23 @@ import org.json.JSONObject;
  * Created by akshayrajgollahalli on 5/10/15.
  */
 public class JSON {
-    public static void main(String[] args) {
+    public String[] returner() {
+
+        String[] result = new String[4];
+
         GetRepo getRepo = new GetRepo();
         String jsonString = getRepo.returner();
 
         JSONObject jsonObject = new JSONObject(jsonString);
         JSONObject jsonArray = jsonObject.getJSONArray("assets").getJSONObject(0);
-        System.out.println(jsonArray.getString("browser_download_url"));
+        result[0] = jsonArray.getString("browser_download_url");
 
-        System.out.println(jsonObject.getString("tag_name"));
-        System.out.println(jsonObject.getString("body"));
+        result[1] = jsonObject.getString("tag_name");
+        result[2] = jsonObject.getString("body");
 
         JSONObject jsonArray1 = jsonObject.getJSONArray("assets").getJSONObject(0);
-        System.out.println(jsonArray1.getString("name"));
+        result[3] = jsonArray1.getString("name");
 
+        return result;
     }
 }
