5b968de3b9c8e19bfec83a93cafe427fbc822dbb
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-10-05 20:07:05 +1300

    support for properties added

---
+++ src/properties/PropertiesWriter.java
@@ -0,0 +1,42 @@
+package properties;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by akshayrajgollahalli on 2/10/15.
+ */
+public class PropertiesWriter {
+    String name;
+    String currentVersion;
+    String newVersion;
+    String summary;
+
+    public PropertiesWriter(String name, String currentVersion, String newVersion, String summary) {
+        this.name = name;
+        this.currentVersion = currentVersion;
+        this.newVersion = newVersion;
+        this.summary = summary;
+
+        Properties properties = new Properties();
+
+        properties.setProperty("Name", this.name);
+        properties.setProperty("CurrentVersion", this.currentVersion);
+        properties.setProperty("NewVersion", this.newVersion);
+        properties.setProperty("Summary", this.summary);
+
+
+        try {
+            File file = new File("GUpdater.properties");
+
+            FileOutputStream fileOutputStream = null;
+            fileOutputStream = new FileOutputStream(file);
+            properties.store(fileOutputStream, "Your GUpdater properties");
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
