b6b5e834dc52abefa5752486161c14b89c4ad81a
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-12-24 23:52:59 +1300

    mac app killer added

---
+++ src/com/gollahalli/manager/AppKill.java
@@ -0,0 +1,28 @@
+package com.gollahalli.manager;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by akshayrajgollahalli on 24/12/15.
+ */
+public class AppKill {
+
+    public static void main(String[] args) {
+        try {
+            String line;
+            String test = null;
+            Process p = Runtime.getRuntime().exec("pgrep -lf JCal.app");
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                test = line;
+            }
+
+            Process kill = Runtime.getRuntime().exec("kill -9 " + (test != null ? test.split(" ") : new String[0])[0]);
+            input.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+}
